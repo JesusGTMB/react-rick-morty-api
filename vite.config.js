ce5a1b383feba8efa00fc3e56e2ba657aff47718
JesusGTMB <136134499+JesusGTMB@users.noreply.github.com>
2023-08-26 17:12:47 -0400

    add url repo

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://jesusgtmb.github.io/react-rick-morty-api/',
   plugins: [react()],
 })
